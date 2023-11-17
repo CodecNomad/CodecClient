746a21c4fa23dc65ad42cb94b696d32d458ccc06
CodecNomad <100487345+CodecNomad@users.noreply.github.com>
2023-11-17 22:30:45 +0100

    Fixed EntityEsp

--- src/main/java/com/github/codecnomad/codecclient/modules/EntityEsp.java
+++ src/main/java/com/github/codecnomad/codecclient/modules/EntityEsp.java
@@ -1,85 +1,78 @@
 package com.github.codecnomad.codecclient.modules;
 
-import com.github.codecnomad.codecclient.CodecClient;
 import com.github.codecnomad.codecclient.Config;
 import com.github.codecnomad.codecclient.Module;
 import com.github.codecnomad.codecclient.utils.RenderUtils;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.Vec3;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.commons.lang3.StringUtils;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class EntityEsp extends Module {
+    private final HashSet<Entity> entityList = new HashSet<>();
+
     @SubscribeEvent
-    public void renderTick(RenderWorldLastEvent event) {
-        for (Entity entity : CodecClient.mc.theWorld.loadedEntityList) {
-            if (entity == CodecClient.mc.thePlayer || entity.getName() == null) {
+    public void worldLoad(WorldEvent.Load event) {
+        entityList.clear();
+        for (Entity entity : event.world.loadedEntityList) {
+            checkAndAdd(entity);
+        }
+    }
+
+    @SubscribeEvent()
+    public void entityDeath(LivingDeathEvent event) {
+        entityList.remove(event.entity);
+    }
+
+    @SubscribeEvent()
+    public void entitySpawn(EntityJoinWorldEvent event) {
+        checkAndAdd(event.entity);
+    }
+
+    @SubscribeEvent()
+    public void renderLast(RenderWorldLastEvent event) {
+        final HashSet<Entity> copiedEntityList = new HashSet<>(entityList);
+        for (Entity entity : copiedEntityList) {
+            if (!entity.isEntityAlive()) {
+                entityList.remove(entity);
                 continue;
             }
-
-            String entityName = entity.getName().toLowerCase();
-            String customName = entity.getCustomNameTag();
-            boolean hasCustomName = entity.hasCustomName();
-
-            boolean isWhitelisted = false;
-
-            if (!Config.EntityEspWhitelist.isEmpty()) {
-                for (String whitelist : Config.EntityEspWhitelist.split(";")) {
-                    if (entityName.contains(whitelist.toLowerCase()) ||
-                            (hasCustomName && customName.toLowerCase().contains(whitelist.toLowerCase()))) {
-                        isWhitelisted = true;
-                        break;
-                    }
-                }
-            }
-            else {
-                isWhitelisted = true;
-            }
-
-            if (!isWhitelisted) {
-                continue;
-            }
-
             GlStateManager.disableDepth();
-            AxisAlignedBB alignedBB = new AxisAlignedBB(
-                    entity.posX - 0.5,
-                    entity.posY,
-                    entity.posZ - 0.5,
-                    entity.posX + 0.5,
-                    entity instanceof EntityArmorStand ? entity.posY - 2 : entity.posY + 2,
-                    entity.posZ + 0.5
-            );
-            RenderUtils.drawOutlinedFilledBoundingBox(alignedBB, Color.blue, event.partialTicks);
-
-            String displayName = "§bName: §4" + entity.getName().replaceAll("§[A-Za-z]", "").replaceAll("§[0-9]", "");
-            RenderUtils.draw3DString(
-                    new Vec3(
-                            entity.posX,
-                            entity instanceof EntityArmorStand ? entity.posY - 1 + 0.2 : entity.posY + 1 + 0.2,
-                            entity.posZ
-                    ),
-                    displayName,
-                    255,
-                    event.partialTicks
-            );
-            String distance = "§bDistance: §4" + (int) entity.getDistanceToEntity(CodecClient.mc.thePlayer) + " §bblocks";
-            RenderUtils.draw3DString(
-                    new Vec3(
-                            entity.posX,
-                            entity instanceof EntityArmorStand ? entity.posY - 1 : entity.posY + 1,
-                            entity.posZ
-                    ),
-                    distance,
-                    255,
-                    event.partialTicks
+            AxisAlignedBB entityBB = entity.getEntityBoundingBox();
+            RenderUtils.drawOutlinedFilledBoundingBox(
+                new AxisAlignedBB(
+                    entityBB.minX - Config.EntityEspWidth,
+                    entityBB.minY,
+                    entityBB.minZ - Config.EntityEspWidth,
+                    entityBB.maxX + Config.EntityEspWidth,
+                    entityBB.maxY,
+                    entityBB.maxZ + Config.EntityEspWidth
+                ),
+                Color.getHSBColor(Config.EntityEspColorH, Config.EntityEspColorS, Config.EntityEspColorB),
+                event.partialTicks
             );
             GlStateManager.enableDepth();
         }
     }
-}
 
+    private void checkAndAdd(Entity entity) {
+        String entityName = entity.getName();
+
+        for (String name : Config.EntityEspWhitelist.split(";")) {
+            if (StringUtils.containsIgnoreCase(entityName, name)) {
+                entityList.add(entity);
+                break;
+            }
+        }
+    }
+}
