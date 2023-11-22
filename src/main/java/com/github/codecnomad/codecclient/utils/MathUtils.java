b4112bf17cbd471e32c8c4887cc380b9e9ffa032
CodecNomad <100487345+CodecNomad@users.noreply.github.com>
2023-11-22 20:39:39 +0100

    Revert Math

--- src/main/java/com/github/codecnomad/codecclient/utils/MathUtils.java
+++ src/main/java/com/github/codecnomad/codecclient/utils/MathUtils.java
@@ -5,24 +5,19 @@ import net.minecraft.util.BlockPos;
 
 public class MathUtils {
 
-    public static float humanEaseInOut(float progress) {
-        float easedProgress = progress * progress * (3.0f - 2.0f * progress);
-
-        float breathing = 0.1f * (float)Math.sin(progress * Math.PI);
-        float humanizedProgress = easedProgress + breathing;
-
-        return Math.min(1.0f, Math.max(0.0f, humanizedProgress));
+    public static float easeInOut(float t) {
+        return t * t * (3.0f - 2.0f * t);
     }
     public static float interpolate(float goal, float current, float time) {
-        float t = humanEaseInOut(time);
+        float t = easeInOut(time);
         return current + (goal - current) * t;
     }
 
-    public static float getYaw(BlockPos p) {
+    float getYaw(BlockPos p) {
         return (float) Math.toDegrees(Math.atan2(p.getZ(), p.getX()));
     }
 
-    public static float getPitch(BlockPos p) {
+    float getPitch(BlockPos p) {
         return (float) Math.toDegrees(-Math.asin(p.getY() / CodecClient.mc.thePlayer.getDistanceSq(p)));
     }
 }
